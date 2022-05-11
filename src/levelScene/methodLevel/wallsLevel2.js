6914428d3fb4450c37b485fa3e82bf3bc1757a7e
TomRuisseau <tom.ruisseau@student.junia.com>
2022-05-11 11:05:23 +0200

    debut des murs

---
+++ src/levelScene/methodLevel/wallsLevel2.js
@@ -0,0 +1,8 @@
+const ww = window.innerWidth;
+const wh = window.innerHeight;
+
+export const walls = [
+    [ww * (5.5/24), wh *  (1/8) ,    ww * (11/24),  wh * (2/8) ],
+    [ww * (1.5/24), wh *  (5/8) ,    ww * (3/24),   wh * (6/8) ],
+    [ww * (3.5/24), wh *  (6.5/16) , ww * (2.5/24), wh * (1.5/16) ],
+];
