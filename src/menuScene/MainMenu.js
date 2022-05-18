6516b665f4fcf1502462eea5cfb69e0403cc4324
SimonHauguel <simon.hauguel@student.junia.com>
2022-05-18 13:36:36 +0200

    remove button from menu

--- src/menuScene/MainMenu.js
+++ src/menuScene/MainMenu.js
@@ -34,54 +34,9 @@ export default class MainMenu extends Phaser.Scene {
     this.allBackground.forEach((i) => (i.visible = false));
     this.allBackground[(this.n = 0)].visible = true;
 
-    this.selectedButton = 0;
-    this.lastShiftDownTime = 0;
-
-    const { width, height } = this.scale;
-
-    this.playButton = this.add.rectangle(
-      width / 2,
-      height / 8,
-      200,
-      100,
-      0x0000ff,
-      1
-    );
-    this.playText = this.add
-      .text(this.playButton.x, this.playButton.y, "Play", { color: "#000000" })
-      .setOrigin(0.5);
-
-    this.settingsButton = this.add.rectangle(
-      width / 3,
-      (6 * height) / 7,
-      200,
-      100,
-      0xffffff,
-      1
-    );
-    this.settingsText = this.add
-      .text(this.settingsButton.x, this.settingsButton.y, "Settings", {
-        color: "#000000",
-      })
-      .setOrigin(0.5);
-
-    this.creditButton = this.add.rectangle(
-      (2 * width) / 3,
-      (6 * height) / 7,
-      200,
-      100,
-      0xffffff,
-      1
-    );
-
-    this.creditText = this.add
-      .text(this.creditButton.x, this.creditButton.y, "Credit", {
-        color: "#000000",
-      })
-      .setOrigin(0.5);
 
     this.keyBoard = this.input.keyboard;
-    this.inputKeysMeta = this.keyBoard.addKeys("UP,DOWN,ENTER");
+    this.inputKeysMeta = this.keyBoard.addKeys("ENTER");
   }
 
   update() {
@@ -90,53 +45,6 @@ export default class MainMenu extends Phaser.Scene {
     this.allBackground.forEach((i) => (i.visible = false));
     this.allBackground[Math.floor(this.n / 10)].visible = true;
 
-    if (
-      this.inputKeysMeta.UP.isDown &&
-      this.inputKeysMeta.UP.timeDown - this.lastShiftDownTime > 100
-    ) {
-      this.lastShiftDownTime = this.inputKeysMeta.UP.timeDown;
-      switch (this.selectedButton) {
-        case 1:
-          this.playButton.setFillStyle(0x0000ff, 1);
-          this.settingsButton.setFillStyle(0xffffff, 1);
-          this.selectedButton -= 1;
-          break;
-        case 2:
-          this.settingsButton.setFillStyle(0x0000ff, 1);
-          this.creditButton.setFillStyle(0xffffff, 1);
-          this.selectedButton -= 1;
-          break;
-      }
-    }
-
-    if (
-      this.inputKeysMeta.DOWN.isDown &&
-      this.inputKeysMeta.DOWN.timeDown - this.lastShiftDownTime > 100
-    ) {
-      this.lastShiftDownTime = this.inputKeysMeta.DOWN.timeDown;
-      switch (this.selectedButton) {
-        case 0:
-          this.settingsButton.setFillStyle(0x0000ff, 1);
-          this.playButton.setFillStyle(0xffffff, 1);
-          this.selectedButton += 1;
-          break;
-        case 1:
-          this.creditButton.setFillStyle(0x0000ff, 1);
-          this.settingsButton.setFillStyle(0xffffff, 1);
-          this.selectedButton += 1;
-          break;
-      }
-    }
-
-    if (this.inputKeysMeta.ENTER.isDown && this.selectedButton === 0)
-      this.scene.start("LevelSecond");
-    else if (this.inputKeysMeta.ENTER.isDown && this.selectedButton === 1)
-      this.scene.start("SettingsMenu");
-  }
-
-  anyOfKey(keys, duration = 0) {
-    for (let key of keys)
-      if (this.keyBoard.checkDown(key, duration)) return true;
-    return false;
+    if (this.inputKeysMeta.ENTER.isDown) this.scene.start("LevelFirst");
   }
 }
